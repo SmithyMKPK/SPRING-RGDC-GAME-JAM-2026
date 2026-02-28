662ff52b72e21921d3a694039cfa0647af2ccef5
geekazodium <59382423+geekazodium@users.noreply.github.com>
2026-02-28 16:58:58 -0500

    feature: camera works

--- levels/light/lightsource.gd
+++ levels/light/lightsource.gd
@@ -8,5 +8,9 @@ class_name LightSource
 		self.global_light_material.set("shader_parameter/max_distance", strength);
 @export var global_light_material: ShaderMaterial;
 
+func _ready() -> void:
+	if !Engine.is_editor_hint():
+		self.strength = self.strength;
+
 func _process(_delta: float) -> void:
 	self.global_light_material.set("shader_parameter/vision_light_source", self.global_position);
