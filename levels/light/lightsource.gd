14034252b89fad62fd57a1061b1fe6a32ae8c6b2
geekazodium <59382423+geekazodium@users.noreply.github.com>
2026-02-28 15:03:40 -0500

    feature: lightsource and autotile

---
+++ levels/light/lightsource.gd
@@ -0,0 +1,12 @@
+@tool
+extends Node2D
+class_name LightSource
+
+@export var strength: float = 300:
+	set(value):
+		strength = value;
+		self.global_light_material.set("shader_parameter/max_distance", strength);
+@export var global_light_material: ShaderMaterial;
+
+func _process(_delta: float) -> void:
+	self.global_light_material.set("shader_parameter/vision_light_source", self.global_position);
